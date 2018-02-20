d67d3fc043e03b64340f23820c3cb2407b36eeed
Yondonjamts Ganzorig <yondonjamts@outlook.com>
2018-02-20 14:00:08 +0800

    added comment between tasks

--- lab2.sql
+++ lab2.sql
@@ -1,21 +1,22 @@
+--task 1
 SELECT * FROM staff 
 WHERE position1 IN ("Manager" ,"Supervisor");
-
+--task 2
 SELECT * FROM staff
 WHERE sex = "F"
 ORDER BY dob DESC;
-
+--task 3
 SELECT * FROM propertyforrent
 WHERE rooms BETWEEN 3 AND 5
 ORDER BY rent;
-
+--task 4
 SELECT fName, lName, telNo FROM client1
 WHERE fName LIKE 'M%';
-
+--task 6
 SELECT street,city,rooms,rent
 FROM propertyforrent
 WHERE staffNo NOT LIKE ''
 ORDER BY rent ASC;
-
+--task 7
 SELECT * FROM privateowner 
 ORDER BY privateowner.fName ASC;
