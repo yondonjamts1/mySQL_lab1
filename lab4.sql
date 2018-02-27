dcef35b24f3a2eee2ef474440d13bfbffc956984
Yondonjamts Ganzorig <yondonjamts@outlook.com>
2018-02-27 14:53:50 +0800

    added task #3

--- lab4.sql
+++ lab4.sql
@@ -5,3 +5,8 @@ group by city;
 --task 2
 SELECT sex, count(sex), max(salary), min(salary) from staff
 group by sex;
+
+--task 3
+select position1,sex,count(sex) from staff
+group by position1,sex
+order by count(sex) desc;
