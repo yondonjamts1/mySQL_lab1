f01e9b7366e57f9f924f95cbe806c044e1559999
Yondonjamts Ganzorig <yondonjamts@outlook.com>
2018-02-27 12:59:38 +0800

    added task #3

--- lab3.sql
+++ lab3.sql
@@ -5,3 +5,8 @@ where position1 = 'Assistant';
 --task 2
 SELECT * FROM staff WHERE sex = "M" ORDER BY dob ASC
 
+--task 3
+select * from propertyforrent
+where rooms between 3 and 4
+order by rent ASC;
+
